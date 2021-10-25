40d242c55744aa20fa860f1d123bd98cfaa94f0e
Ilya Kosorukov <ephiros@pop-os.localdomain>
2021-10-25 16:48:04 +0100

    feat: basic parser

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,1 +1,7 @@
+from lark import Lark
 
+with open("grammar.lark",encoding="utf-8") as grammar:
+    LP = Lark(grammar.read(),start="sentence")
+
+tree = LP.parse("cat /path/ & cd /path/")
+print(tree.pretty())
