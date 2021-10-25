b8d01e216ad6ef1e60aa9a8be0ca57ae234381e2
Adi Bozzhanov <bozzhanov.at@gmail.com>
2021-10-26 00:59:17 +0100

    One more

--- doc/src/shell.md
+++ doc/src/shell.md
@@ -1,2 +1,3 @@
 Module src.shell
 ================
+The main loop of our shell is here
--- src/shell.py
+++ src/shell.py
@@ -1,1 +1,10 @@
+"""
+The main loop of our shell is here
+"""
 
+if __name__ == "__main__":
+
+    while true:
+        text = input()
+
+        print(text)
