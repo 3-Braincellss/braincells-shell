3c608344b5451cd044873b6c94de8d065fe58fff
sudoPom <72107751+sudoPom@users.noreply.github.com>
2021-11-10 17:44:06 +0000

    Fix: Fixed echo implementation to use args dict.

--- src/apps/echo.py
+++ src/apps/echo.py
@@ -6,14 +6,13 @@ class EchoApp(App):
     """
 
     def __init__(self, args):
-
-        pass
+        self.args = args
 
     def run(self, text):
         """
         """
+        text = self.args["value"]
         return text
 
     def validate_args(self):
-
         pass
