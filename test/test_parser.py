35e25fef075a270c7d11c64c2af3fbbcbd2402e0
Ilya Kosorukov <ephiros@pop-os.localdomain>
2021-11-11 12:30:44 +0000

    random parser tests (not finished)

---
+++ test/test_parser.py
@@ -0,0 +1,20 @@
+import unittest
+from parser import parser
+
+
+class TestShell(unittest.TestCase):
+    def test_parser(self):
+        tree = parser.run_parser("echo -f stuff --flag stuff2")
+        print(tree.pretty())
+        tree = parser.run_parser("echo /path/")
+        print(tree.pretty())
+        tree = parser.run_parser("echo /sdf/ > 'text.txt'")
+        print(tree.pretty())
+        tree = parser.run_parser("echo \"\'double quoted quote\'\"")
+        print(tree.pretty())
+        tree = parser.run_parser("echo \"this is space: `echo \"quotedthing\"`\"")
+        print(tree.pretty())
+
+
+if __name__ == "__main__":
+    unittest.main()
