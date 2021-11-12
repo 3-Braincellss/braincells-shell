a1e6ff963cff6dff909b552d6af4066bbb31ab1f
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-11-12 14:56:43 +0000

    Feat: Added option validation.

--- src/apps/pwd.py
+++ src/apps/pwd.py
@@ -1,19 +1,26 @@
 from apps.app import App
+from exceptions.app_context import AppContextException
+from exceptions.app_run import AppRunException
 import os
 
 class PwdApp(App):
     """
     """
 
+    allowed_options = {"-L", "-P"}
+
     def __init__(self, args):
-        ## TODO: handle options 
+        ## TODO: handle options
+        self.args = args
         pass
 
     def run(self, inp):
         """
         """
+        self.validate_args()
         return os.path.abspath(".") + "\n"
 
     def validate_args(self):
-
-        pass
+        for option in self.args:
+            if option not in PwdApp.allowed_options:
+                raise AppRunException("pwd", f"{option}: invalid option")
