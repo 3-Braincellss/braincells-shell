ca7ed23ce9912bdf92a4252e16373b9616028194
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-11-15 10:53:35 +0000

    Feat: Option validation

--- src/apps/cut.py
+++ src/apps/cut.py
@@ -16,3 +16,12 @@ class CutApp(App):
     def validate_args(self):
 
         pass
+    def validate_args(self):
+        if not self.options:
+            raise AppRunException(
+                "cut", "Missing option: -b byte_cut_start-byte_cut_end,.. >=[")
+        if len(self.options) != 1:
+            raise AppRunException("cut", "Invalid number of options >=[")
+        if self.options[0][0] != '-b':
+            raise AppRunException(
+                "cut", f"Invalid option: {self.options[0][0]}")
