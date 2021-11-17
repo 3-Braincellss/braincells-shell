b6a09b1830abe562d855ac63d773d48f415344d3
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-11-17 00:37:48 +0000

    Feat: Created method to read text from file.

--- src/common/tools.py
+++ src/common/tools.py
@@ -7,3 +7,15 @@ def prettify_path(path):
         words[i] = words[i][0]
     ret = "/".join(words)
     return ret
+
+def read_from_file(path, app_str):
+    """
+    Reads from a file and returns its contents
+    :param path: The path of the file to be read.
+    """
+    try:
+        with open(path, "r") as f:
+            text = f.read()
+            return text
+    except FileNotFound:
+        raise AppRunException(app_str, f"{path}: No such file or directory")
