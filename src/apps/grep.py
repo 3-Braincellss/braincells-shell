bb5fd7d44b3d12d48c162b5a6bb8f105462a0058
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-11-22 14:18:13 +0000

    Feat: Grep functionality added.

--- src/apps/grep.py
+++ src/apps/grep.py
@@ -1,18 +1,38 @@
-from apps.app import App
+from apps import App
+from getopt import getopt
+from exceptions import AppRunException
+from common.tools import read_lines_from_file
+import re
 
 
 class GrepApp(App):
     """
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, args):
+        self.option, self.args = getopt(args, "")
 
-    def run(self):
+    def run(self, inp, out):
         """
         """
-        pass
+        self.pattern = self.args[0]
+        if len(self.args) == 1:
+            self._run([input()], out)
+            return out
+        paths = self.args[1:]
+        for path in paths:
+            contents = read_lines_from_file(path, "grep")
+            self._run(contents, out)
+        return out
+
+    def _run(self, lines, out):
+        for line in lines:
+            if re.match(self.pattern, line):
+                x = line
+                if x[-1] != "\n":
+                    x += "\n"
+                out.append(x)
 
     def validate_args(self):
-
-        pass
+        if not self.args:
+            raise AppRunException("grep", "Usage: grep [PATTERN] [FILE]...")
