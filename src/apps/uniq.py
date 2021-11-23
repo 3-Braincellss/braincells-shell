d789e1294fc323590f11d8f353b0154a670f7f79
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-11-23 00:05:49 +0000

    Feat: Implemented uniq app

--- src/apps/uniq.py
+++ src/apps/uniq.py
@@ -1,18 +1,47 @@
-from apps.app import App
-
+from apps import App
+from getopt import getopt
+from exceptions import AppRunException
+from common.tools import read_lines_from_file
 
 class UniqApp(App):
     """
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, args):
+        self.options, self.args = getopt(args, "i")
 
-    def run(self):
+    def run(self, inp, out):
         """
         """
-        pass
+        case = False if self.options else True
+        if not self.args:
+            contents = [input()]
+        else:
+            contents = read_lines_from_file(self.args[0])
+        if case:
+            self._run_unsensitive(contents, out)
+        else:
+            self._run(contents, out)
+        return out
+
+    def _run(self, lines, out):
+        out.append(lines[0])
+        prev = lines[0]
+        for i in range(1,len(lines)):
+            if lines[i] != prev:
+                prev = lines[i]
+                out.append(lines[i])
+
+    def _run_unsensitive(self, lines, out):
+        out.append(lines[0])
+        prev = lines[0]
+        for i in range(1,len(lines)):
+            if lines[i].upper() != prev.upper():
+                prev = lines[i]
+                out.append(lines[i])
+
 
     def validate_args(self):
-
+        if len(self.args) > 1:
+            raise AppRunException("uniq", "too many arguments")
         pass
