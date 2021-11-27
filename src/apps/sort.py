42c4b082110dfb8962887d5bb79c4be04def33d0
sudoPom <72107751+sudoPom@users.noreply.github.com>
2021-11-27 21:55:30 +0000

    Fix: Sort no longer has pwd (what?)

--- src/apps/sort.py
+++ src/apps/sort.py
@@ -1,5 +1,3 @@
-from apps.app import App
-from exceptions import AppRunException
 import os
 
 from getopt import getopt, GetoptError
@@ -21,23 +19,6 @@ class SortApp(App):
 
     FILE is the name of the file. If not specified, uses stdin.
     """
-
-
-class PwdApp(App):
-    """ """
-
-    allowed_options = {"-L", "-P"}
-
-    def __init__(self, args):
-        # TODO: handle options
-        self.args = args
-        pass
-
-    def run(self, inp, out):
-        """ """
-        self.validate_args()
-        out.append(os.path.abspath(".") + "\n")
-        return out
 
     def __init__(self, args):
         self.args = args
