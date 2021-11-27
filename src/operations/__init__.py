fbd2d2e2d0d47a7c55df3b3ced3f16af7169a8d2
adi <bozzhanov.at@gmail.com>
2021-11-27 19:53:38 +0000

    feat: redirects init

--- src/operations/__init__.py
+++ src/operations/__init__.py
@@ -3,3 +3,5 @@ from .call import Call
 from .pipe import Pipe
 from .sequence import Sequence
 from .operation_factory import OperationFactory
+from .l_redirect import LeftRedirect
+from .r_redirect import RightRedirect
