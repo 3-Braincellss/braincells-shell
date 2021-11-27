d065812e53a64be1240894f43d016217c8fa8746
adi <bozzhanov.at@gmail.com>
2021-11-27 20:24:25 +0000

    fix: redirects

--- src/operations/call.py
+++ src/operations/call.py
@@ -7,9 +7,28 @@ from apps import AppFactory
 
 class Call(Operation):
     def __init__(self, data):
-        self.app = AppFactory.get_app(data["app"], data["args"])
+        self.app = AppFactory.get_app(
+            data["app"],
+            data["args"],
+        )
+        self.left_red = data["left_red"]
+        self.right_red = data["right_red"]
 
     def run(self, inp, out):
+        _inp = inp
+        _out = out
+        if self.left_red is not none and not inp:
+            lines = deque(read_lines_from_file(self.left_red, "redirect"))
+            _inp = deque(lines)
+
+        if self.right_red is not None:
+            _out = deque()
+            with open(self.fname, "w") as f:
+                out = self.op.run(inp, out)
+                for line in out:
+                    f.write(line)
+                    f.write("\n")
+
         self.app.validate_args()
-        out = self.app.run(inp, out)
+        out = self.app.run(_inp, _out)
         return out
