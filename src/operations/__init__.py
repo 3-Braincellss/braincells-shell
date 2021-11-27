90a08ad9c3e7ec45f6ad738541595da9df862010
adi <bozzhanov.at@gmail.com>
2021-11-27 20:00:04 +0000

    fix: commmmmmmmmooon

--- src/operations/__init__.py
+++ src/operations/__init__.py
@@ -2,6 +2,6 @@ from .operation import Operation
 from .call import Call
 from .pipe import Pipe
 from .sequence import Sequence
-from .operation_factory import OperationFactory
 from .l_redirect import LeftRedirect
 from .r_redirect import RightRedirect
+from .operation_factory import OperationFactory
