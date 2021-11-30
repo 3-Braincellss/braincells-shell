2ef5e3d17ac657b7796693c407524829a7d3e112
Adi <bozzhanov.at@gmail.com>
2021-11-30 03:31:53 +0000

    feat: beautified the code and refactored exceptions a bit

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,3 +1,1 @@
-from .app_context import AppContextException
-from .app_not_found import AppNotFoundException
-from .app_run import AppRunException
+from .exceptions import *
--- src/exceptions/app_not_found.py
+++
@@ -1,5 +0,0 @@
-class AppNotFoundException(Exception):
-    def __init__(self, app_str):
-        self.app_str = app_str
-        self.message = f"Command '{self.app_str}' doesn't exist. Sorry :( \n"
-        super().__init__(self.message)
