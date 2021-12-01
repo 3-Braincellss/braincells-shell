530c0d83e20fc434060fc5eab2123dc0a83836b9
adi <bozzhanov.at@gmail.com>
2021-12-01 14:21:41 +0000

    feat: pylint exceptions 10/10

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,1 +1,15 @@
-from .exceptions import *
+"""
+Exceptions:
+
+This module contains exceptions that shell will be raising and handling.
+There is one common interface ``AppExcpetion`` and 3 concrete implementations
+of that interface.
+
+"""
+
+from .exceptions import (
+    AppException,
+    AppContextException,
+    AppRunException,
+    AppNotFoundException,
+)
--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -1,3 +1,8 @@
+"""
+Here you can find all of our exception types and the definition of our
+commmon exception interface.
+"""
+
 from abc import abstractmethod, ABCMeta
 
 
@@ -7,7 +12,8 @@ class AppException(Exception, metaclass=ABCMeta):
     Some exceptions that are raised within our shell are expected behaviour.
     App exceptions will only stop or prevent apps from running.
     They will not cause the whole shell process to stop.
-    All app exceptions are aware of the app that caused an exception and aware of an appropriate message to show.
+    All app exceptions are aware of the app that caused an exception
+    and aware of an appropriate message to show.
     These exceptions are handled on the highest level of command execution.
 
     Attributes:
@@ -28,8 +34,9 @@ class AppContextException(AppException):
     Each app has a `validate_args()` method that is run right before the app is run.
     This method will check options that are passed to the app
     and raise `AppContextException` if they don't make sense.
-    `AppContextException` exists to prevent apps from running when they are doomed to fail
-    based on options alone, which should hopefully prevent wasting some CPU power when it's unnecessary.
+    `AppContextException` exists to prevent apps from running
+    when they are doomed to fail based on options alone,
+    which should hopefully prevent wasting some CPU power when it's unnecessary.
 
     These errors **WILL** be ignored with **unsafe** apps.
     """
@@ -42,8 +49,9 @@ class AppContextException(AppException):
 class AppNotFoundException(AppException):
     """Errors that occur when shell doesn't know the app you are trying to run
 
-    These errors are called by the `AppFactory`, in case it is not aware of the app you are trying to run.
-    Using an unsafe signature will not prevent this error from appearing.
+    These errors are called by the `AppFactory`, in case it is not aware of
+    the app you are trying to run. Using an unsafe signature will not
+    prevent this error from appearing.
 
     """
 
