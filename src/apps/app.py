15d134c7548517365ae30e56c4fcf0c6ec4606a0
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-01 10:44:14 +0000

    Feat: Pylint app 10/10

--- src/apps/app.py
+++ src/apps/app.py
@@ -1,24 +1,33 @@
-"""
-This module provides an interface for an App.
-"""
+"""This module provides an interface for an App."""
 from abc import abstractmethod, ABCMeta
 
 
 class App(metaclass=ABCMeta):
+    """An abstract class representing the format of all Apps.
+
+    Args:
+        args (:obj: `list`): Contains all the arguments and options of the application.
     """
-    An abstract class representing the format of all Apps.
-    """
+
+    def __init__(self, args):
+        pass
 
     @abstractmethod
     def validate_args(self):
         """
         Checks whether the given args are appropriate for the application.
         """
-        pass
 
     @abstractmethod
     def run(self, inp, out):
+        """Executes the application on the given arguments.
+
+        Args:
+            inp (:obj: `deque`, optional): The input args of the command, only used for piping
+                and redirects.
+            out (:obj: `deque`): The output deque, used to store the result of execution.
+
+        Returns:
+            ``deque``: The deque filled with the results of application execution.
+
         """
-        Runs the application.
-        """
-        pass
