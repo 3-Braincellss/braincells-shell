e4e00932288d1bc20c5a84810435d4ed386f465a
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-07 13:11:01 +0000

    Feat: Testing * Pwd 100%

--- test/apps/test_pwd.py
+++ test/apps/test_pwd.py
@@ -1,10 +1,19 @@
 from hypothesis import given, strategies as st
 import unittest
+from apps import PwdApp
+from exceptions import ContextError
+import os
 
 
 class TestPwd(unittest.TestCase):
-    pass
 
+    def test_pwd(self):
+        expected = os.path.abspath(".")
+        out = []
+        PwdApp([]).run(None, out)
+        self.assertEqual(out[0], expected)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_giving_args_raises_error(self):
+        args = ["-e", "I", "will", "throw", "an", "error.txt"]
+        with self.assertRaises(ContextError):
+            PwdApp(args).run(None, [])
