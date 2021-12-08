e6528fd2fd0ac0cb1fed8c7f73f78666b8f9a17e
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-08 23:20:06 +0000

    Fix: Changed colour to light cyan

--- src/shellprompt/shellsession.py
+++ src/shellprompt/shellsession.py
@@ -23,7 +23,7 @@ class ShellSession(PromptSession):
             "user_host": "#ff006e",
             "path": "#06d6a0",
             "arrow": "#118ab2",
-            "pygments.keyword": "#118ab2",
+            "pygments.keyword": "#a5f3fc",
         })
         self.lexer = PygmentsLexer(KeyWordLexer)
         super().__init__(style=self.style, completer=ShellPathCompleter(), lexer=self.lexer)
