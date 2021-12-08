0433b11a0cb12f0aa8e89ec346b4baa8432b09c3
Adi <bozzhanov.at@gmail.com>
2021-12-08 22:59:11 +0000

    fix: made the completer not complete the first word

--- src/shellprompt/shellcompleter.py
+++ src/shellprompt/shellcompleter.py
@@ -4,8 +4,11 @@ from prompt_toolkit.document import Document
 
 class ShellPathCompleter(PathCompleter):
     def get_completions(self, document, complete_event):
-
-        new_text = document.text.split(" ")[-1]
+        words = document.text.split(" ")
+        if len(words) <= 1:
+            return []
+            
+        new_text = words[-1]
         new_cursor = len(new_text)
         new_document = Document(new_text, new_cursor)
 
