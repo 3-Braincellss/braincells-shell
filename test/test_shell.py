5b9037e588bbe4426f0faa48201928daff62ee5f
adi <bozzhanov.at@gmail.com>
2021-12-09 16:56:05 +0000

    fix: shell tests updated

--- test/test_shell.py
+++ test/test_shell.py
@@ -1,7 +1,7 @@
 import unittest
 
 from shell_test_interface import ShellTestCase
-from shell import Shell
+from shell import execute
 from exceptions import (
     AppNotFoundError,
     ContextError,
@@ -14,30 +14,25 @@ class TestShell(ShellTestCase):
     def test_execute_simple(self):
 
         command_text = "echo hello"
-        out = Shell.execute(command_text)
-        result = "\n".join(out)
+        out = execute(command_text)
 
-        self.assertEqual(result, "hello")
+        self.assertEqual(out, "hello")
 
     def test_run_app_not_found(self):
-        sh = Shell()
 
         with self.assertRaises(AppNotFoundError):
-            sh.run("gcc make file")
+            execute("gcc make file")
 
     def test_run_context(self):
-        sh = Shell()
 
         with self.assertRaises(ContextError):
-            sh.run("ls one two three")
+            execute("ls one two three")
 
     def test_run_app_run_error(self):
-        sh = Shell()
 
         with self.assertRaises(RunError):
-            sh.run("cat wakanadaaaaaaaaaaaaaaaaa")
+            execute("cat wakanadaaaaaaaaaaaaaaaaa")
 
     def test_run_syntax_error(self):
-        sh = Shell()
         with self.assertRaises(ShellSyntaxError):
-            sh.run(";;;")
+            execute(";;;")
