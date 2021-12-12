acd3e347b9167cc672e8186d40b54cee0a9e3333
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-12 19:57:42 +0000

    Fix: Gave Echo a more appropriate name

--- test/test_echo.py
+++ test/test_echo.py
@@ -5,7 +5,7 @@ from apps import EchoApp
 
 class TestEcho(unittest.TestCase):
     @given(st.from_regex("[a-zA-Z0-9 ]*", fullmatch=True))
-    def test_echo_alpha_num(self, text):
+    def test_echo_never_fails(self, text):
         app = EchoApp(text.split(" "))
         out = []
         app.run(None, out)
