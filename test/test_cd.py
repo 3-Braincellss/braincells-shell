75e6abb1dd00c6fc0071222d02773b5ad042d644
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-12 01:54:31 +0000

    Testing: Cd 100% Coverafe

--- test/test_cd.py
+++ test/test_cd.py
@@ -1,13 +1,37 @@
 from hypothesis import given, strategies as st
 import unittest
 from shell_test_interface import ShellTestCase
+from exceptions import ContextError, RunError
 from apps import CdApp
 import os
+
+TEST_PATH = os.getcwd() + "/_test"
 
 
 class TestCd(ShellTestCase):
 
+    def tearDown(self):
+        os.chdir(TEST_PATH)
+        super().tearDown()
+
     def test_cd_no_args_goes_home(self):
         app = CdApp([])
         app.run([], [])
-        print(os.getcwd())
+        self.assertEqual(os.path.expanduser("~"), os.getcwd())
+
+    def test_change_dir(self):
+        app = CdApp(["dir_empty"])
+        app.run([], [])
+        self.assertEqual(TEST_PATH + "/dir_empty", os.getcwd())
+
+    def test_too_many_args_raises_exception(self):
+        with self.assertRaises(ContextError):
+            CdApp(["dir_empty", "dir_files"]).validate_args()
+
+    def test_directory_does_not_exist(self):
+        with self.assertRaises(ContextError):
+            CdApp(["what_is_my_purpose"]).validate_args()
+
+    def test_cd_into_file_raises_exception(self):
+        with self.assertRaises(RunError):
+            CdApp(["dir_files/file_1"]).run([], [])
