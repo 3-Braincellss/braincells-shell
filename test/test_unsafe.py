329448b6d7cc835bc9013499ce4f8e033ed5fb1d
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-13 00:39:51 +0000

    Testing: Unsafe 100% Coverage * Could probably add some more tests, but this is okay for now.

--- test/test_unsafe.py
+++ test/test_unsafe.py
@@ -1,6 +1,27 @@
-from hypothesis import given, strategies as st
 import unittest
+from apps import UnsafeApp
+from apps import LsApp
+from exceptions import ContextError
+from shell_test_interface import ShellTestCase
+import os
 
 
-class TestUnsafe(unittest.TestCase):
-    pass
+class TestUnsafe(ShellTestCase):
+
+    def test_unsafe_no_error(self):
+        expected = sorted(["dir_empty", "dir_files", "dir_nested",
+                           "dir_out", "no_extension", "other_extension.py", "toplevel.txt"])
+        out = []
+        app = LsApp([])
+        unsafe_app = UnsafeApp(app)
+        unsafe_app.run(None, out)
+        self.assertEqual(out, expected)
+
+    def test_unsafe_no_error(self):
+        expected = [
+            "(\'ls\', \"[Errno 2] No such file or directory: \'where_am_i\'\")"]
+        out = []
+        app = LsApp(["where_am_i"])
+        unsafe_app = UnsafeApp(app)
+        unsafe_app.run(None, out)
+        self.assertEqual(out, expected)
