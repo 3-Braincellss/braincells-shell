fc020f7a0e2750d6900c5180a6012aee8d447b76
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-13 00:40:19 +0000

    Testing: Common Tools 100% Coverage

---
+++ test/test_tools.py
@@ -0,0 +1,33 @@
+import unittest
+from shell_test_interface import ShellTestCase
+from common.tools import *
+from exceptions import RunError
+
+
+class TestTools(ShellTestCase):
+
+    def test_read_contents(self):
+        expected = "AAA\nBBB\nDDD\nI don't know any more letters"
+        out = read_from_file("dir_files/file-5", "tool_test")
+        self.assertEqual(out, expected)
+
+    def test_reading_contents_from_non_existant_file_raises_exception(self):
+        with self.assertRaises(RunError):
+            out = read_from_file("dir_files/file-6", "tool_test")
+
+    def test_reading_contents_from_a_directory_raises_exception(self):
+        with self.assertRaises(RunError):
+            out = read_from_file("dir_files", "tool_test")
+
+    def test_read_lines(self):
+        expected = ["AAA\n", "BBB\n", "DDD\n", "I don't know any more letters"]
+        out = read_lines_from_file("dir_files/file-5", "tool_test")
+        self.assertEqual(out, expected)
+
+    def test_reading_lines_from_non_existant_file_raises_exception(self):
+        with self.assertRaises(RunError):
+            out = read_lines_from_file("dir_files/file-6", "tool_test")
+
+    def test_reading_lines_from_a_directory_raises_exception(self):
+        with self.assertRaises(RunError):
+            out = read_lines_from_file("dir_files", "tool_test")
