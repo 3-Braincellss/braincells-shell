af182bebaff8aa5eb4985a0c6c6f8d0d104765d3
Oluwaponmile Femi-Sunmaila <pponmilee@gmail.com>
2021-12-13 20:41:01 +0000

    Fix: Hypothesis no longer uses hypothesis.

--- test/test_cut.py
+++ test/test_cut.py
@@ -1,9 +1,6 @@
-from hypothesis import given
-from hypothesis import strategies as st
 from shell_test_interface import ShellTestCase
 
 from apps import CutApp
-from common.tools import read_lines_from_file
 from exceptions import ContextError, RunError
 
 IPSUM_MAX = 592
@@ -11,136 +8,63 @@ TEST_TEXT_PATH = "./dir_files/file-5"
 
 
 class TestCut(ShellTestCase):
-    @given(st.integers(1, IPSUM_MAX), st.integers(1, IPSUM_MAX))
-    def test_cut(self, x, y):
-        interval = (min(x, y), max(x, y))
-        lines = read_lines_from_file(TEST_TEXT_PATH, "cut_test")
-        expected = [
-            (lambda line: line[interval[0] - 1:interval[1]].rstrip())(line)
-            for line in lines
-        ]
-        args = ["-b", f"{interval[0]}-{interval[1]}", TEST_TEXT_PATH]
-        app = CutApp(args)
+    def test_cut(self):
+        expected = ["AAA", "BBB", "DDD", "I d"]
+        app = CutApp(["-b", "1-3", TEST_TEXT_PATH])
+        app.validate_args()
         out = []
-        out = app.run(None, out)
+        app.run(None, out)
         self.assertEqual(out, expected)
 
-    @given(st.integers(1, IPSUM_MAX))
-    def test_cut_open_start_interval(self, x):
-        lines = read_lines_from_file(TEST_TEXT_PATH, "cut_test")
-        expected = [(lambda line: line[:x].rstrip())(line) for line in lines]
-        args = ["-b", f"-{x}", TEST_TEXT_PATH]
-        app = CutApp(args)
+    def test_cut_open_start_interval(self):
+        expected = ["AAA", "BBB", "DDD", "I d"]
+        app = CutApp(["-b", "-3", TEST_TEXT_PATH])
         out = []
-        out = app.run(None, out)
+        app.run(None, out)
         self.assertEqual(out, expected)
 
-    @given(st.integers(1, IPSUM_MAX))
-    def test_cut_open_end_interval(self, x):
-        lines = read_lines_from_file(TEST_TEXT_PATH, "cut_test")
-        expected = [(lambda line: line[x - 1:].rstrip())(line)
-                    for line in lines]
-        args = ["-b", f"{x}-", TEST_TEXT_PATH]
-        app = CutApp(args)
+    def test_cut_open_end_interval(self):
+        expected = ["AA", "BB", "DD", " don't know any more letters"]
+        app = CutApp(["-b", "2-", TEST_TEXT_PATH])
         out = []
-        out = app.run(None, out)
+        app.run(None, out)
         self.assertEqual(out, expected)
 
-    @given(
-        st.lists(st.integers(1, IPSUM_MAX),
-                 min_size=1,
-                 max_size=IPSUM_MAX,
-                 unique=True))
-    def test_cut_individual_interval(self, x):
-        string_intervals = [str(val) for val in x]
-        lines = read_lines_from_file(TEST_TEXT_PATH, "cut_test")
-
-        expected = self.my_cut(lines, set(x))
-
-        args = ["-b", f"{','.join(string_intervals)}", TEST_TEXT_PATH]
+    def test_cut_individual_interval(self):
+        expected = ["AA", "BB", "DD", "Idn"]
         out = []
-        CutApp(args).run(None, out)
+        CutApp(["-b", "1,3,5", TEST_TEXT_PATH]).run(None, out)
         self.assertEqual(out, expected)
 
     def test_cut_open_interval(self):
-        lines = read_lines_from_file(TEST_TEXT_PATH, "cut_test")
-        expected = [(lambda line: line.rstrip())(line) for line in lines]
-        args = ["-b", "-", TEST_TEXT_PATH]
-        app = CutApp(args)
+        expected = ["AAA", "BBB", "DDD", "I don't know any more letters"]
         out = []
-        out = app.run(None, out)
+        CutApp(["-b", "-", TEST_TEXT_PATH]).run(None, out)
         self.assertEqual(out, expected)
 
-    @given(st.integers(1, IPSUM_MAX), st.integers(1, IPSUM_MAX))
-    def test_cut_input_redirection(self, x, y):
-        interval = (min(x, y), max(x, y))
-        lines = read_lines_from_file(TEST_TEXT_PATH, "cut_test")
-        expected = [
-            (lambda line: line[interval[0] - 1:interval[1]].rstrip())(line)
-            for line in lines
-        ]
-        args = ["-b", f"{interval[0]}-{interval[1]}"]
-        app = CutApp(args)
+    def test_cut_input_redirection(self):
+        expected = ["AA", "BB", "DD", "Idn"]
         out = []
-        out = app.run(lines, out)
+        CutApp(["-b", "1,3,5"]).run(["AAA", "BBB", "DDD",
+                                     "I don't know any more letters"], out)
         self.assertEqual(out, expected)
 
-    @given(st.integers(1, IPSUM_MAX), st.integers(1, IPSUM_MAX))
-    def test_decreasing_interval_raises_exception(self, x, y):
-        if x == y:
-            return
-        interval = (max(x, y), min(x, y))
-        args = ["-b", f"{interval[0]}-{interval[1]}", TEST_TEXT_PATH]
-        app = CutApp(args)
+    def test_decreasing_interval_raises_exception(self):
         with self.assertRaises(RunError):
-            app.run(None, [])
+            CutApp(["-b", "12-9", TEST_TEXT_PATH]).run(None, [])
 
-    @given(st.integers(1, IPSUM_MAX), st.integers(1, IPSUM_MAX),
-           st.integers(1, IPSUM_MAX))
-    def test_invalid_interval_raises_exception(self, x, y, z):
-        if x == y:
-            return
-        interval = [x, y, z]
-        args = [
-            "-b", f"{interval[0]}-{interval[1]}-{interval[2]}", TEST_TEXT_PATH
-        ]
-        app = CutApp(args)
+    def test_invalid_interval_raises_exception(self):
         with self.assertRaises(RunError):
-            app.run(None, [])
+            CutApp(["-b", "1-2-3", TEST_TEXT_PATH]).run(None, [])
 
-    @given(st.integers(1, IPSUM_MAX), st.floats(1, IPSUM_MAX))
-    def test_non_int_interval_raises_exception(self, x, y):
-        if x == y:
-            return
-        interval = (min(x, y), max(x, y))
-        args = ["-b", f"{interval[0]}-{interval[1]}", TEST_TEXT_PATH]
-        app = CutApp(args)
+    def test_non_int_interval_raises_exception(self):
         with self.assertRaises(RunError):
-            app.run(None, [])
+            CutApp(["-b", f"2.0-4", TEST_TEXT_PATH]).run(None, [])
 
-    @given(st.integers(0, 0), st.integers(1, IPSUM_MAX))
-    def test_interval_starting_with_zero_raises_exception(self, x, y):
-        if x == y:
-            return
-        interval = (x, y)
-        args = ["-b", f"{interval[0]}-{interval[1]}", TEST_TEXT_PATH]
-        app = CutApp(args)
+    def test_interval_starting_with_zero_raises_exception(self):
         with self.assertRaises(RunError):
-            app.run(None, [])
+            CutApp(["-b", "0-3", TEST_TEXT_PATH]).run(None, [])
 
-    @given(st.from_regex("(-(A|a|[c-z]|[C-Z]))|(^(?![\s\S]))", fullmatch=True))
-    def test_invalid_option_raises_exception(self, char):
-        args = [f"{char}", f"{0}-{10}", TEST_TEXT_PATH]
+    def test_invalid_option_raises_exception(self):
         with self.assertRaises(ContextError):
-            CutApp(args).validate_args()
-
-    @staticmethod
-    def my_cut(lines, intervals):
-        out = []
-        for line in lines:
-            str = ""
-            for pos, char in enumerate(line):
-                if pos + 1 in intervals:
-                    str += char
-            out.append(str.rstrip())
-        return out
+            CutApp(["-s", "2-4", TEST_TEXT_PATH]).validate_args()
